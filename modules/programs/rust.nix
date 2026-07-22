6d6d5581a10bcbf6d04a5b1c98cd1d170ffe7782
keepinfov <218463407+keepinfov@users.noreply.github.com>
2026-07-22 09:44:37 +0300

    Convert rust and virtualization modules to options+mkIf pattern

--- modules/programs/rust.nix
+++ modules/programs/rust.nix
@@ -1,40 +1,49 @@
 # modules/programs/rust.nix - Rust development environment
-{ lib, pkgs, inputs, conf, ... }:
+{ lib, pkgs, config, inputs, conf, ... }:
+
+with lib;
 
 let
+  cfg = config.programs.rust;
   cargoPath = conf.paths.cargoDir;
 in
 {
-  nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
+  options.programs.rust = {
+    enable = mkEnableOption "Rust development toolchain" // { default = true; };
+  };
 
-  environment.systemPackages = [
-    (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-      extensions = [
-        "rust-src"
-        "rust-analyzer"
-        "rustfmt"
-        "clippy"
-        "llvm-tools-preview"
-      ];
-      targets = [
-        "x86_64-unknown-linux-gnu"
-        "x86_64-unknown-linux-musl"
-        "x86_64-pc-windows-gnu"
-        "thumbv6m-none-eabi"      # Cortex-M0/M0+
-        "thumbv7em-none-eabihf"   # Cortex-M4F/M7F
-        "riscv32imac-unknown-none-elf"  # RISC-V
+  config = mkIf cfg.enable {
+    nixpkgs.overlays = [ inputs.rust-overlay.overlays.default ];
+
+    environment.systemPackages = [
+      (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+        extensions = [
+          "rust-src"
+          "rust-analyzer"
+          "rustfmt"
+          "clippy"
+          "llvm-tools-preview"
+        ];
+        targets = [
+          "x86_64-unknown-linux-gnu"
+          "x86_64-unknown-linux-musl"
+          "x86_64-pc-windows-gnu"
+          "thumbv6m-none-eabi"      # Cortex-M0/M0+
+          "thumbv7em-none-eabihf"   # Cortex-M4F/M7F
+          "riscv32imac-unknown-none-elf"  # RISC-V
 		"aarch64-linux-android"
 		"armv7-linux-androideabi"
 		"i686-linux-android"
 		"x86_64-linux-android"
-      ];
-    }))
-    pkgs.cargo-cross
-    pkgs.cargo-watch
-    pkgs.cargo-expand
-  ];
+        ];
+      }))
+      pkgs.cargo-cross
+      pkgs.cargo-watch
+      pkgs.cargo-expand
+    ];
 
-  # Add cargo to PATH
-  environment.variables.PATH = "${cargoPath}:$PATH";
-  environment.sessionVariables.PATH = "${cargoPath}:$PATH";
+    # Add cargo to PATH
+    environment.variables.PATH = "${cargoPath}:$PATH";
+    environment.sessionVariables.PATH = "${cargoPath}:$PATH";
+  };
 }
--- modules/programs/virtualization.nix
+++ modules/programs/virtualization.nix
@@ -1,25 +1,36 @@
 # modules/programs/virtualization.nix - Docker, VirtualBox
 { lib, pkgs, config, ... }:
 
+with lib;
+
+let
+  cfg = config.programs.virtualization;
+in
 {
-  virtualisation = {
-    docker = {
-      enable = true;
-      enableOnBoot = true;
-    };
-    
-    # virtualbox = {
-    #   host = {
-    #     enable = true;
-    #     enableExtensionPack = true;
-    #   };
-    # };
+  options.programs.virtualization = {
+    enable = mkEnableOption "Virtualization support (Docker, OBS, etc.)" // { default = true; };
   };
-  programs.obs-studio = {
-    enable = true;
-    enableVirtualCamera = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      droidcam-obs
-    ];
+
+  config = mkIf cfg.enable {
+    virtualisation = {
+      docker = {
+        enable = true;
+        enableOnBoot = true;
+      };
+
+      # virtualbox = {
+      #   host = {
+      #     enable = true;
+      #     enableExtensionPack = true;
+      #   };
+      # };
+    };
+    programs.obs-studio = {
+      enable = true;
+      enableVirtualCamera = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        droidcam-obs
+      ];
+    };
   };
 }
